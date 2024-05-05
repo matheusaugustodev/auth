189eaddaee7851ab11435662d1df6cf740ebac28
matheusaugustodev <matheus4medeiros4@gmail.com>
2024-05-05 15:26:39 -0300

    add query

--- app.js
+++ app.js
@@ -14,7 +14,14 @@ const APP_PORT = process.env.PORT || 8080
 const APP_URL = process.env.URL || `http://localhost:${APP_PORT}`
 
 
-app.get('/', (req, res) => res.json({ message: 'Hello World!' }))
+app.get('/', async (req, res) => {
+
+    // select * from users
+    const users = await sql`select * from users`
+    
+    res.json({ users })
+        
+})
 
 app.listen(APP_PORT, () => console.info(`Servidor rodando em ${APP_URL}`))
 
