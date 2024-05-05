38478d3a701ac588d4589df5ea25a40394b41878
matheusaugustodev <matheus4medeiros4@gmail.com>
2024-05-05 15:10:55 -0300

    add supabase

--- app.js
+++ app.js
@@ -1,33 +1,17 @@
 const express = require('express')
 const { config } = require('dotenv')
-const { MongoClient } = require('mongodb')
+const postgres = require('postgres')
 
 config()
+
+const connectionString = process.env.SUPABASE_URI
+const sql = postgres(connectionString)
 
 const app = express()
 app.use(express.json());
 
 const APP_PORT = process.env.PORT || 8080
 const APP_URL = process.env.URL || `http://localhost:${APP_PORT}`
-
-async function connectToCluster(uri) {
-    let mongoClient;
- 
-    try {
-        mongoClient = new MongoClient(uri);
-        console.log('Connecting to MongoDB Atlas cluster...');
-        await mongoClient.connect();
-        console.log('Successfully connected to MongoDB Atlas!');
- 
-        return mongoClient;
-    } catch (error) {
-        console.error('Connection to MongoDB Atlas failed!', error);
-        process.exit();
-    }
- }
-
-const uri = process.env.MONGODB_URI
-const client = connectToCluster(uri)
 
 
 app.get('/', (req, res) => res.json({ message: 'Hello World!' }))
